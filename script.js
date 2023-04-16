603ecba5ebc9123b57ce5a004b021198fd93c6d0
gigson2 <2gigson@gmail.com>
2023-04-16 13:36:51 +0000

    Javascript updated

--- script.js
+++ script.js
@@ -1,8 +1,102 @@
+//Object
+
+let person = {
+    name: 'Twumasi',
+    age: 51
+};
+//Dot notation
+person.name
+
+//Bracket notation
+person['age']
+console.log(person.age);
+
+//Array
+let Selcolors = ['Blue', 'White'];
+Selcolors[2] = 2;
+console.log(Selcolors.length);
+console.log(Selcolors);
+
+//function a task
+function la() {
+    console.log('Hello World');
+}
+la();
+
+//function with parameter performing a  task
+function la(age, namee) {
+    console.log('Hello World' + ' ' + age + ' ' + namee);
+}
+la('40', 'Boadi');
+
+// calc a value
+function sq(number) {
+    return number * number;
+}
+let number = 5;
+console.log(sq(6));
+
+//arithmetic operations
+let num1 = 19;
+let num2 = 4;
+
+console.log(num1 + num2);
+console.log(num1 - num2);
+console.log(num1 / num2);
+console.log(num1 * num2);
+console.log(num1 ** num2);
+console.log(num1 % num2);
+console.log(num1++);
+console.log(num2--);
+console.log(++num1);
+console.log(--num2);
+
+//comparison operator = true & false
+//relational operators
+let comp1 = 5;
+console.log(comp1 > 0);
+console.log(comp1 < 0);
+console.log(comp1 >= 0);
+console.log(comp1 <= 0);
+console.log(comp1 == 0);
+console.log(comp1 != 0);
+//strict equality operator same type and value
+console.log(1 === 1);
+console.log('1' === 1);
+
+//lose equality same value
+console.log(1 == 1);
+console.log('1' == 1);
+
+//ternary operators
+// if a customer has more than 100 points they are gold operators
+// others they are silver operators
+let point = 105;
+let type = point > 100 ? 'Gold Customer' : 'Silver';
+console.log(type);
+
+//Logical AND (&&)
+//it returns true if both operands are true
+let gih = true;
+let gif = true;
+
+let fig = gif && gih;
+console.log('picture format', fig);
+//logical OR (||)
+//it returns true if one operand is true
+let gihf = true;
+let gift = true;
+
+let figt = gift && gihf;
+console.log('picture format', figt);
+//NOT(!);
+let pdff = !gift;
+console.log('not picture format', pdff);
 //logical operators
 let usercol = 'Green';
-let defacol='White';
+let defacol = 'White';
 
-let choscol= usercol||defacol;
+let choscol = usercol || defacol;
 console.log(choscol);
 
 //Bitwise operators
@@ -10,22 +104,45 @@ let a = 'Red';
 let b = 'Green';
 
 let c = a;
-a=b;
-b=c;
+a = b;
+b = c;
 console.log(a);
 console.log(b);
 //if statement
-let hour =0;
-if (hour>=6 && hour<=12) {
+let hour = 0;
+if (hour >= 6 && hour <= 12)
     console.log('Good Morning');
-}
-else if (hour>=12 && hour<=18) {
+else if (hour >= 12 && hour <= 18)
     console.log('Good Afternoon');
-}
-else if (hour>=18 && hour<=24) {
+else if (hour >= 18 && hour <= 24)
     console.log('Good evening');
-}
-else{
+else
     console.log('Good Night');
-}
+
 //swtich & case
+let role = 'Admin';
+switch (role) {
+    case 'guest':
+        console.log('Guest User');
+        break;
+    case 'User 1':
+        console.log('User 1');
+        break;
+    case 'Admin':
+        console.log('Admin User');
+        break;
+    default:
+        console.log('Unknown User');
+}
+
+//LOOPS STATEMENTS
+//---- For(initialExpression; condition; incrementExpression){
+// statement}
+//
+for (let i = 0; i <= 5; i++) {
+    console.log('Hello WordPress', i);
+}
+// printing odd number
+for (let fi = 0; fi <= 5; fi++) {
+    if (fi % 2 !== 0) console.log(fi);
+}
